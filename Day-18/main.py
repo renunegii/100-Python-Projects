97032b0a207fc80866b913bfed544d1b66e81701
renunegii <officialrenu1@gmail.com>
2025-10-18 23:51:43 +0530

    made a square with turtle movements

--- Day-18/main.py
+++ Day-18/main.py
@@ -3,9 +3,10 @@ from turtle import Turtle, Screen
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("green")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
-timmy_the_turtle.forward(100)
+# Creating a square using turtle functions
+for i in range (4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.right(90)
 
 screen = Screen()
 screen.exitonclick()
