dca245bfd97754d10b0571429777a1471ceb2ba7
renunegii <officialrenu1@gmail.com>
2025-10-19 00:04:37 +0530

    made a dashed line with turtle

--- Day-18/main.py
+++ Day-18/main.py
@@ -4,9 +4,16 @@ timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
 timmy_the_turtle.color("green")
 # Creating a square using turtle functions
-for i in range (4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.right(90)
+# for i in range (4):
+#     timmy_the_turtle.forward(100)
+#     timmy_the_turtle.right(90)
+
+# creating a dashed line
+for _ in range(15):
+    timmy_the_turtle.pendown()
+    timmy_the_turtle.forward(10)
+    timmy_the_turtle.penup()
+    timmy_the_turtle.forward(10)
 
 screen = Screen()
 screen.exitonclick()
