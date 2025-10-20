f2585a154b8fab0958971ca6c43ba3a581d6d2d6
renunegii <officialrenu1@gmail.com>
2025-10-20 17:23:49 +0530

    reating an overlay of different shapes

--- Day-18/main.py
+++ Day-18/main.py
@@ -9,11 +9,19 @@ timmy_the_turtle.color("green")
 #     timmy_the_turtle.right(90)
 
 # creating a dashed line
-for _ in range(15):
-    timmy_the_turtle.pendown()
-    timmy_the_turtle.forward(10)
-    timmy_the_turtle.penup()
-    timmy_the_turtle.forward(10)
+# for _ in range(15):
+#     timmy_the_turtle.pendown()
+#     timmy_the_turtle.forward(10)
+#     timmy_the_turtle.penup()
+#     timmy_the_turtle.forward(10)
+
+# creating an overlay of different shapes
+
+for i in range (3,10):
+    angle = 360/i
+    for j in range(i):
+        timmy_the_turtle.forward(100)
+        timmy_the_turtle.right(angle)
 
 screen = Screen()
 screen.exitonclick()
