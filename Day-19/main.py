ec50ca716ec9a74839f107514048b18b2332e860
renunegii <officialrenu1@gmail.com>
2025-10-27 22:06:32 +0530

    Updated Turtle event listeners

--- Day-19/main.py
+++ Day-19/main.py
@@ -1,2 +1,22 @@
-#Python high order functions and event listeners
-#Python Turtle High order functions and Event Listener Practicing Programs
+#Python High Order Functions and Event Listeners
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+def move_forward():
+    tim.forward(40)
+
+def move_left():
+    tim.left(90)
+    tim.forward(40)
+
+def move_right():
+    tim.right(90)
+    tim.forward(40)
+
+screen.listen()
+screen.onkey(move_forward, "space")
+screen.onkey(move_left, "1")
+screen.onkey(move_right, "2")
+screen.exitonclick()
