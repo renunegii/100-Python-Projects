1b2a56fd7360c54c9aa333409ca7b35d094fcfa6
renunegii <officialrenu1@gmail.com>
2025-10-31 23:52:43 +0530

    Practicing Event Listeners

--- Day-19/main.py
+++ Day-19/main.py
@@ -1,4 +1,5 @@
 #Python High Order Functions and Event Listeners
+import turtle
 from turtle import Turtle, Screen
 
 tim = Turtle()
@@ -15,8 +16,15 @@ def move_right():
     tim.right(90)
     tim.forward(40)
 
+def move_in_circle():
+    # turtle.pensize(50)
+    # turtle.pencolor("red")
+    tim.circle(50)
+
+
 screen.listen()
 screen.onkey(move_forward, "space")
 screen.onkey(move_left, "1")
 screen.onkey(move_right, "2")
+screen.onkey(move_in_circle, "space")
 screen.exitonclick()
