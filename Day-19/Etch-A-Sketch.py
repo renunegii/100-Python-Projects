1c0a5d80ac3eab3fa942d66263be03c39d17cbd6
renunegii <officialrenu1@gmail.com>
2025-11-01 22:25:13 +0530

    Etch-a-sketch added

---
+++ Day-19/Etch-A-Sketch.py
@@ -0,0 +1,27 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+def move_forward():
+    tim.forward(40)
+
+def move_left():
+    tim.left(90)
+    tim.forward(40)
+
+def move_right():
+    tim.right(90)
+    tim.forward(40)
+
+def move_in_circle():
+    # turtle.pensize(50)
+    # turtle.pencolor("red")
+    tim.circle(50)
+
+
+screen.listen()
+screen.onkey(move_forward, "space")
+screen.onkey(move_left, "1")
+screen.onkey(move_right, "2")
+screen.onkey(move_in_circle, "space")
+screen.exitonclick()
