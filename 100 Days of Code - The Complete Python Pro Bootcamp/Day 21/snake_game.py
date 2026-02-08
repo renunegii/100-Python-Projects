8c2767890854e05d06fae06e25a75953d169bfe1
renunegii <officialrenu1@gmail.com>
2026-02-08 21:24:58 +0530

    Added Snake to move in a particular direction

--- 100 Days of Code - The Complete Python Pro Bootcamp/Day 21/snake_game.py
+++ 100 Days of Code - The Complete Python Pro Bootcamp/Day 21/snake_game.py
@@ -1,16 +1,37 @@
+from time import sleep
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(600,600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+screen.tracer(0)
 
-snake_pos = [0,-20,-40]
+snake_pos = [-40,-20,0]
+snake_segments=[]
 for i in range(0,3):
-    new_turtle = Turtle()
-    new_turtle.shape("square")
-    new_turtle.color("white")
-    new_turtle.goto(snake_pos[i],0)
+    segment = Turtle()
+    segment.shape("square")
+    segment.color("white")
+    segment.penup()
+    segment.goto(snake_pos[i], 0)
+    snake_segments.append(segment)
+
+is_game_on = True
+while is_game_on:
+    screen.update()
+    time.sleep(0.1)
+    for seg_num in range (len(snake_segments)-1,0,-1):
+        new_x = snake_segments[seg_num-1].xcor()
+        new_y = snake_segments[seg_num-1].ycor()
+        snake_segments[seg_num].goto(new_x,new_y)
+    snake_segments[0].forward(20)
+    snake_segments[0].left(90)
+
+
+
+
 
 
 
